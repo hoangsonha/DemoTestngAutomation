cdefe8c6b36058d6a3d2cf7ff8579b6ddf5ea192
hoangsonha <hahsse160429@fpt.edu.vn>
2024-06-11 20:04:04 +0700

    first commit

---
+++ src/main/java/com/hsh/demo/main/DemoTestNGAutomation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hsh.demo.main;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class DemoTestNGAutomation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/hsh/demo/login/UserNGTest.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package com.hsh.demo.login;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class UserNGTest {
+    
+    WebDriver driver;
+    
+    @BeforeMethod
+    public void setUp() {
+        // driverManager quản lí driver của chúng ta (trong th này là chromedriver())
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://testngautomation.azurewebsites.net/");
+    }
+    
+    @Test
+    public void testTableSortAndSearch() {
+        
+       driver.findElement(By.linkText("Login")).click();
+       driver.findElement(By.id("email")).sendKeys("hoangsonha492@gmail.com");
+       driver.findElement(By.id("password")).sendKeys("1"); 
+       driver.findElement(By.id("b_submit")).click();   
+       String actualValue = driver.findElement(By.id("message")).getText();    
+       String expectedValue = "Login Successfully !!!";      
+       Assert.assertEquals(actualValue, expectedValue);
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+//        driver.quit();
+    }
+    
+}
