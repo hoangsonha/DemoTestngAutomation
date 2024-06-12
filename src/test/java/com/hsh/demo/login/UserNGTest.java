cb4332b4e8a2c228f4b338b88443334e59ab2568
hoangsonha <hahsse160429@fpt.edu.vn>
2024-06-12 22:25:23 +0700

    update project

--- src/test/java/com/hsh/demo/login/UserNGTest.java
+++ src/test/java/com/hsh/demo/login/UserNGTest.java
@@ -32,10 +32,11 @@ public class UserNGTest {
     }
     
     @Test
-    public void testLoginWithUserAndPasswordIsCorrect() {
+    public void testLoginWithUserAndPasswordIsCorrect() throws InterruptedException {
        driver.findElement(By.linkText("Login")).click();
        driver.findElement(By.id("email")).sendKeys("hoangsonha492@gmail.com");
        driver.findElement(By.id("password")).sendKeys("1"); 
+       Thread.sleep(5000);
        driver.findElement(By.id("b_submit")).click();   
        String actualValue = driver.findElement(By.id("message")).getText();    
        String expectedValue = "Login Successfully!!!";    
@@ -43,10 +44,11 @@ public class UserNGTest {
     }
     
     @Test
-    public void testLoginWithUserAndPasswordNotCorrect() {
+    public void testLoginWithUserAndPasswordNotCorrect() throws InterruptedException {
        driver.findElement(By.linkText("Login")).click();
        driver.findElement(By.id("email")).sendKeys("hoangsonha492@gmail.com");
        driver.findElement(By.id("password")).sendKeys("123"); 
+       Thread.sleep(5000);
        driver.findElement(By.id("b_submit")).click();   
        String actualValue = driver.findElement(By.id("message")).getText();    
        String expectedValue = "Login Successfully!!!";      
@@ -54,19 +56,155 @@ public class UserNGTest {
     }
     
     @Test
-    public void testLoginWithUserNotCorrectAndPasswordNotCorrect() {
+    public void testLoginWithUserNotCorrectAndPasswordNotCorrect() throws InterruptedException {
        driver.findElement(By.linkText("Login")).click();
-       driver.findElement(By.id("email")).sendKeys("hoangsonha492fdasdsa@gmail.com");
+       driver.findElement(By.id("email")).sendKeys("@gmail.com");
        driver.findElement(By.id("password")).sendKeys("123"); 
+       Thread.sleep(5000);
        driver.findElement(By.id("b_submit")).click();   
        String actualValue = driver.findElement(By.id("message")).getText();    
        String expectedValue = "Login Successfully!!!";      
        Assert.assertNotEquals(actualValue, expectedValue);
     }
     
+    @Test
+    public void testLoginWithNoData() throws InterruptedException {
+       driver.findElement(By.linkText("Login")).click();
+       driver.findElement(By.id("email")).sendKeys("");
+       driver.findElement(By.id("password")).sendKeys(""); 
+       Thread.sleep(5000);
+       driver.findElement(By.id("b_submit")).click();   
+       String actualValue = driver.findElement(By.id("message")).getText();    
+       String expectedValue = "Login Successfully!!!";      
+       Assert.assertNotEquals(actualValue, expectedValue);
+    }
+    
+    
+    
+    @Test
+    public void testRegisterWithNoDataOfEmail() throws InterruptedException {
+       driver.findElement(By.linkText("Register")).click();
+       driver.findElement(By.id("email")).sendKeys("");
+       driver.findElement(By.id("password")).sendKeys("1");  
+       driver.findElement(By.id("firstName")).sendKeys("Hoang");
+       driver.findElement(By.id("lastName")).sendKeys("Ha");
+       driver.findElement(By.id("phone")).sendKeys("0334386995");
+       driver.findElement(By.id("address")).sendKeys("Da Lat");
+       Thread.sleep(5000);
+       driver.findElement(By.id("b_submit")).click(); 
+       
+       String actualValue = driver.findElement(By.id("message")).getText();    
+       String expectedValue = "User registered successfully!!!";  
+       Assert.assertNotEquals(actualValue, expectedValue);
+    }
+    
+    @Test
+    public void testRegisterWithNoDataOfPassword() throws InterruptedException {
+       driver.findElement(By.linkText("Register")).click();
+       driver.findElement(By.id("email")).sendKeys("nguoisaohoa@gmail.com");
+       driver.findElement(By.id("password")).sendKeys("");  
+       driver.findElement(By.id("firstName")).sendKeys("Hoang");
+       driver.findElement(By.id("lastName")).sendKeys("Ha");
+       driver.findElement(By.id("phone")).sendKeys("0334386995");
+       driver.findElement(By.id("address")).sendKeys("Da Lat");
+       Thread.sleep(5000);
+       driver.findElement(By.id("b_submit")).click(); 
+       
+       String actualValue = driver.findElement(By.id("message")).getText();    
+       String expectedValue = "User registered successfully!!!";  
+       Assert.assertNotEquals(actualValue, expectedValue);
+    }
+    
+    @Test
+    public void testRegisterWithNoDataOfFirstName() throws InterruptedException {
+       driver.findElement(By.linkText("Register")).click();
+       driver.findElement(By.id("email")).sendKeys("nguoisaohoa@gmail.com");
+       driver.findElement(By.id("password")).sendKeys("1");  
+       driver.findElement(By.id("firstName")).sendKeys("");
+       driver.findElement(By.id("lastName")).sendKeys("Ha");
+       driver.findElement(By.id("phone")).sendKeys("0334386995");
+       driver.findElement(By.id("address")).sendKeys("Da Lat");
+       Thread.sleep(5000);
+       driver.findElement(By.id("b_submit")).click(); 
+       
+       String actualValue = driver.findElement(By.id("message")).getText();    
+       String expectedValue = "User registered successfully!!!";  
+       Assert.assertNotEquals(actualValue, expectedValue);
+    }
+    
+    @Test
+    public void testRegisterWithNoDataOfLastName() throws InterruptedException {
+       driver.findElement(By.linkText("Register")).click();
+       driver.findElement(By.id("email")).sendKeys("nguoisaohoa@gmail.com");
+       driver.findElement(By.id("password")).sendKeys("1");  
+       driver.findElement(By.id("firstName")).sendKeys("Hoang");
+       driver.findElement(By.id("lastName")).sendKeys("");
+       driver.findElement(By.id("phone")).sendKeys("0334386995");
+       driver.findElement(By.id("address")).sendKeys("Da Lat");
+       Thread.sleep(5000);
+       driver.findElement(By.id("b_submit")).click();  
+       
+       String actualValue = driver.findElement(By.id("message")).getText();    
+       String expectedValue = "User registered successfully!!!";  
+       Assert.assertNotEquals(actualValue, expectedValue);
+    }
+    
+    @Test
+    public void testRegisterWithNoDataOfPhone() throws InterruptedException {
+       driver.findElement(By.linkText("Register")).click();
+       driver.findElement(By.id("email")).sendKeys("nguoisaohoa@gmail.com");
+       driver.findElement(By.id("password")).sendKeys("1");  
+       driver.findElement(By.id("firstName")).sendKeys("Hoang");
+       driver.findElement(By.id("lastName")).sendKeys("Ha");
+       driver.findElement(By.id("phone")).sendKeys("");
+       driver.findElement(By.id("address")).sendKeys("Da Lat");
+       Thread.sleep(5000);
+       driver.findElement(By.id("b_submit")).click(); 
+       
+       String actualValue = driver.findElement(By.id("message")).getText();    
+       String expectedValue = "User registered successfully!!!";  
+       Assert.assertNotEquals(actualValue, expectedValue);
+    }
+    
+    @Test
+    public void testRegisterWithNoDataOfAddress() throws InterruptedException {
+       driver.findElement(By.linkText("Register")).click();
+       driver.findElement(By.id("email")).sendKeys("nguoisaohoa@gmail.com");
+       driver.findElement(By.id("password")).sendKeys("1");  
+       driver.findElement(By.id("firstName")).sendKeys("Hoang");
+       driver.findElement(By.id("lastName")).sendKeys("Ha");
+       driver.findElement(By.id("phone")).sendKeys("0334386995");
+       driver.findElement(By.id("address")).sendKeys("");
+       Thread.sleep(5000);
+       driver.findElement(By.id("b_submit")).click();  
+       
+       String actualValue = driver.findElement(By.id("message")).getText();    
+       String expectedValue = "User registered successfully!!!";  
+       Assert.assertNotEquals(actualValue, expectedValue);
+    }
+    
+    
+    @Test
+    public void testRegisterWithFullData() throws InterruptedException {
+       driver.findElement(By.linkText("Register")).click();
+       driver.findElement(By.id("email")).sendKeys("nguoisaohoa@gmail.com");
+       driver.findElement(By.id("password")).sendKeys("1");  
+       driver.findElement(By.id("firstName")).sendKeys("Hoang");
+       driver.findElement(By.id("lastName")).sendKeys("Ha");
+       driver.findElement(By.id("phone")).sendKeys("0334386995");
+       driver.findElement(By.id("address")).sendKeys("Da Lat");
+       Thread.sleep(5000);
+       driver.findElement(By.id("b_submit")).click();   
+       
+       String actualValue = driver.findElement(By.id("message")).getText();    
+       String expectedValue = "User registered successfully!!!";  
+       Assert.assertEquals(actualValue, expectedValue);
+    }
+    
+    
     @AfterMethod
     public void tearDown() {
-        driver.quit();
+//        driver.quit();
     }
     
 }
